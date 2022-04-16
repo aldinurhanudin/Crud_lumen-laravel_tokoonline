33667b631bdab8f2f896aaf63080f9230d953174
aldi8477 <aldinurhanudin@gmail.com>
2022-04-16 21:59:40 +0700

    lumen

---
+++ web/authors/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller {
+
+    public function showAllAuthors()
+    : JsonResponse {
+
+        $data = Author::all();
+        return response()->json(['message' => 'Success','data' => $data]);
+    }
+
+    public function showOneAuthor($id)
+    : JsonResponse {
+
+        return response()->json(Author::find($id));
+    }
+
+    public function create(Request $request)
+    : JsonResponse {
+
+        $author = Author::create($request->all());
+
+        return response()->json($author, Response::HTTP_CREATED);
+    }
+
+    public function update($id, Request $request)
+    : JsonResponse {
+
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, Response::HTTP_OK);
+    }
+
+    public function delete($id) {
+
+        Author::findOrFail($id)->delete();
+
+        return response('Deleted Successfully', Response::HTTP_OK);
+    }
+}
